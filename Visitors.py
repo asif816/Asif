3e9b9129544e7590a1089efc9a54572260ea0895
asif816 <64182198+asif816@users.noreply.github.com>
2020-04-23 09:39:20 +0600

    Create Visitors.py

---
+++ Visitors.py
@@ -0,0 +1,17 @@
+﻿import webbrowser
+import time
+from pykeyboard import PyKeyboard
+
+count = 0
+urls = ['https://gplinks.co/Zs7Ogkj']
+k = PyKeyboard()
+
+while count 100:
+       for url in urls:
+             webbrowser.open(url, new=0)
+             time.sleep(1 0)
+             k.press_keys(['Command','W'])
+             count = count + 1
+
+else:
+        pass
